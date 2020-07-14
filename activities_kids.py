93a6b186ac557be03ef9cde342e1e8c668f3cf5b
zanebliss <zanebliss@icloud.com>
2020-07-14 14:36:38 -0500

    init commit

---
+++ activities_kids.py
@@ -0,0 +1,21 @@
+def run(*kids):
+    for kid in kids:
+        print(f'{kid} ran like a fool!')
+        
+def swing(*kids):
+    for kid in kids:
+        print(f'{kid} swang like a fool!')
+        
+def slide(*kids):
+    for kid in kids:
+        print(f'{kid} slid like a fool!')
+
+def hide(*kids):
+    for kid in kids:
+        print(f'{kid} hid like a fool!')
+
+run('Pam','Sam','Andrea','Will')
+swing('Marybeth','Ariel','Kevin','Courtney')
+slide('Mike','Jack','Jennifer','Earl')
+hide('Henry','Heather','Haley','Hugh')
+
